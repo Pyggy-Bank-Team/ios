315ab4f0406fe8a5e8f775fc6edb01251b5eb06d
Dave Chupreev <davefirstoff@gmail.com>
2021-04-17 19:47:30 +0300

    Menu bar

--- PiggyBank/Views/BottomBar.swift
+++ PiggyBank/Views/BottomBar.swift
@@ -8,7 +8,7 @@ final class BottomBar: UIView {
         let buttonWidth: CGFloat = 66
         let buttonRadius: CGFloat = buttonWidth / 2
         
-        let halfBeforeFirstArc = (rect.width - buttonWidth) / 2 - 5
+        let halfBeforeFirstArc = (rect.width - buttonWidth) / 2
         let rectHalf: CGFloat = 0
         
         UIColor.white.setFill()
@@ -16,38 +16,40 @@ final class BottomBar: UIView {
         path.addLine(to: .init(x: 0, y: rectHalf))
         path.addLine(to: .init(x: halfBeforeFirstArc, y: rectHalf))
         
-        path.addArc(withCenter: .init(x: halfBeforeFirstArc, y: rectHalf + 5),
-                    radius: 5,
-                    startAngle: 3 * .pi / 2,
-                    endAngle: 2 * .pi,
-                    clockwise: true)
-        
-        path.addArc(withCenter: .init(x: (halfBeforeFirstArc + 5) + buttonRadius, y: rectHalf + 5),
+        path.addArc(withCenter: .init(x: (halfBeforeFirstArc) + buttonRadius, y: rectHalf),
                     radius: buttonRadius,
                     startAngle: .pi,
                     endAngle: 2 * .pi,
                     clockwise: false)
-        
-        path.addArc(withCenter: .init(x: (halfBeforeFirstArc + 5) + buttonWidth + 5, y: rectHalf + 5),
-                    radius: 5,
-                    startAngle: .pi,
-                    endAngle: 3 * .pi / 2,
-                    clockwise: true)
 
         path.addLine(to: .init(x: rect.width, y: rectHalf))
         path.addLine(to: .init(x: rect.width, y: rect.height))
         path.addLine(to: .init(x: 0, y: rect.height))
         path.fill()
         
+        let realButtonWidth: CGFloat = 56
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "add"), for: .normal)
         button.tintColor = .black
         button.backgroundColor = .systemGreen
-        button.frame = CGRect(x: frame.width / 2 - 30,
-                              y: 0 - buttonRadius + 5,
-                              width: 60,
-                              height: 60)
+        button.frame = CGRect(x: (frame.width / 2) - (realButtonWidth / 2),
+                              y: -(realButtonWidth / 2),
+                              width: realButtonWidth,
+                              height: realButtonWidth)
         button.layer.cornerRadius = button.frame.width / 2
+        addSubview(button)
+    }
+    
+    override func layoutSubviews() {
+//        let view = UIView(frame: .init(x: 10, y: 0, width: 20, height: bounds.height))
+//        view.backgroundColor = .red
+//        addSubview(view)
+        
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "ico_bar_accounts_24"), for: .normal)
+        button.setTitle("Accounts", for: .normal)
+        button.tintColor = .black
+        button.frame = CGRect(origin: .zero, size: CGSize(width: 24, height: 24))
         addSubview(button)
     }
 }
