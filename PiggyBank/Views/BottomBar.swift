610aadb604b610a94d9e6c5ea3e3184c323da557
Dave Chupreev <davefirstoff@gmail.com>
2021-04-18 08:28:39 +0300

    Menu (#5)

--- PiggyBank/Views/BottomBar.swift
+++ PiggyBank/Views/BottomBar.swift
@@ -1,14 +1,14 @@
 import UIKit
 
 final class BottomBar: UIView {
-
+    
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         
         let buttonWidth: CGFloat = 66
         let buttonRadius: CGFloat = buttonWidth / 2
         
-        let halfBeforeFirstArc = (rect.width - buttonWidth) / 2 - 5
+        let halfBeforeFirstArc = (rect.width - buttonWidth) / 2
         let rectHalf: CGFloat = 0
         
         UIColor.white.setFill()
@@ -16,38 +16,66 @@ final class BottomBar: UIView {
         path.addLine(to: .init(x: 0, y: rectHalf))
         path.addLine(to: .init(x: halfBeforeFirstArc, y: rectHalf))
         
-        path.addArc(withCenter: .init(x: halfBeforeFirstArc, y: rectHalf + 5),
-                    radius: 5,
-                    startAngle: 3 * .pi / 2,
-                    endAngle: 2 * .pi,
-                    clockwise: true)
-        
-        path.addArc(withCenter: .init(x: (halfBeforeFirstArc + 5) + buttonRadius, y: rectHalf + 5),
+        path.addArc(withCenter: .init(x: (halfBeforeFirstArc) + buttonRadius, y: rectHalf),
                     radius: buttonRadius,
                     startAngle: .pi,
                     endAngle: 2 * .pi,
                     clockwise: false)
-        
-        path.addArc(withCenter: .init(x: (halfBeforeFirstArc + 5) + buttonWidth + 5, y: rectHalf + 5),
-                    radius: 5,
-                    startAngle: .pi,
-                    endAngle: 3 * .pi / 2,
-                    clockwise: true)
 
         path.addLine(to: .init(x: rect.width, y: rectHalf))
         path.addLine(to: .init(x: rect.width, y: rect.height))
         path.addLine(to: .init(x: 0, y: rect.height))
         path.fill()
         
+        let realButtonWidth: CGFloat = 56
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "add"), for: .normal)
         button.tintColor = .black
-        button.backgroundColor = .systemGreen
-        button.frame = CGRect(x: frame.width / 2 - 30,
-                              y: 0 - buttonRadius + 5,
-                              width: 60,
-                              height: 60)
+        button.backgroundColor = UIColor(hexString: "#00B294")
+        button.frame = CGRect(x: (frame.width / 2) - (realButtonWidth / 2),
+                              y: -(realButtonWidth / 2),
+                              width: realButtonWidth,
+                              height: realButtonWidth)
         button.layer.cornerRadius = button.frame.width / 2
         addSubview(button)
+        
+        // Buttons
+        
+        let leftStackView = stackView()
+        addSubview(leftStackView)
+        
+        let rightStackView = stackView()
+        addSubview(rightStackView)
+        
+        leftStackView.addArrangedSubview(MenuButton(image: #imageLiteral(resourceName: "ico_bar_accounts_24"), title: "Accounts"))
+        leftStackView.addArrangedSubview(MenuButton(image: #imageLiteral(resourceName: "ico_bar_reports_24"), title: "Reports"))
+        
+        rightStackView.addArrangedSubview(MenuButton(image: #imageLiteral(resourceName: "ico_bar_operations_24"), title: "Operations"))
+        rightStackView.addArrangedSubview(MenuButton(image: #imageLiteral(resourceName: "ico_bar_profile_24"), title: "Profile"))
+        
+        NSLayoutConstraint.activate([
+            leftStackView.topAnchor.constraint(equalTo: topAnchor),
+            leftStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            leftStackView.leftAnchor.constraint(equalTo: leftAnchor),
+            leftStackView.widthAnchor.constraint(equalToConstant: halfBeforeFirstArc),
+            
+            rightStackView.topAnchor.constraint(equalTo: topAnchor),
+            rightStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            rightStackView.rightAnchor.constraint(equalTo: rightAnchor),
+            rightStackView.widthAnchor.constraint(equalToConstant: halfBeforeFirstArc)
+        ])
+    }
+}
+
+private extension BottomBar {
+    
+    func stackView() -> UIStackView {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
     }
 }
